e975f44402fc42ef5435a2c732879ac23cc8fa78
Alam Wibowo <alamhafidz@gmail.com>
2025-01-30 18:53:30 +0700

    Add Prefix /api

--- src/app.js
+++ src/app.js
@@ -10,7 +10,8 @@ const port = process.env.PORT || 3000;
 const swaggerDocument = yaml.load('./api-spec.yaml');
 
 app.use(express.json());
-app.use('/', routes);
+
+app.use('/api', routes);
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 app.listen(port, () => {
